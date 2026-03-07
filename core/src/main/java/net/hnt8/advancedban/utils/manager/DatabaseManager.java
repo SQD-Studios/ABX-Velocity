41ae64019cf8b14d75ebd63d07d55a883864df10
Chamogelastos <stavros.ssta@gmail.com>
2026-03-07 17:36:58 +0200

    Fixed MySQL It is the most important feature

--- core/src/main/java/net/hnt8/advancedban/utils/manager/DatabaseManager.java
+++ core/src/main/java/net/hnt8/advancedban/utils/manager/DatabaseManager.java
@@ -1,10 +1,9 @@
 package net.hnt8.advancedban.utils.manager;
 
 import com.zaxxer.hikari.HikariDataSource;
-import net.hnt8.advancedban.utils.Universal;
 import net.hnt8.advancedban.utils.DynamicDataSource;
 import net.hnt8.advancedban.utils.SQLQuery;
-import net.hnt8.advancedban.utils.Punishment;
+import net.hnt8.advancedban.utils.Universal;
 
 import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.RowSetFactory;
@@ -15,146 +14,121 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * The Database Manager is used to interact directly with the database is use.<br>
- * Will automatically direct the requests to either MySQL or HSQLDB.
- * <br><br>
- * Looking to request {@link Punishment Punishments} from the Database?
- * Use {@link PunishmentManager#getPunishments(SQLQuery, Object...)} or
- * {@link PunishmentManager#getPunishmentFromResultSet(ResultSet)} for already parsed data.
+ * Robust database manager for AdvancedBanX.
+ * Supports MySQL or embedded HSQLDB.
  */
 public class DatabaseManager {
 
     private HikariDataSource dataSource;
     private boolean useMySQL;
-
     private RowSetFactory factory;
-    
-    private static DatabaseManager instance = null;
 
-    /**
-     * Get the instance of the command manager
-     *
-     * @return the database manager instance
-     */
+    private static DatabaseManager instance;
+
     public static synchronized DatabaseManager get() {
-        return instance == null ? instance = new DatabaseManager() : instance;
+        if (instance == null) {
+            instance = new DatabaseManager();
+        }
+        return instance;
     }
 
     /**
-     * Initially connects to the database and sets up the required tables of they don't already exist.
+     * Initializes the database and ensures tables exist.
      *
-     * @param useMySQLServer whether to preferably use MySQL (uses HSQLDB as fallback)
+     * @param useMySQLServer true to use MySQL, false for HSQLDB
      */
     public void setup(boolean useMySQLServer) {
-        useMySQL = useMySQLServer;
+        this.useMySQL = useMySQLServer;
 
         try {
             dataSource = new DynamicDataSource(useMySQL).generateDataSource();
-        } catch (ClassNotFoundException ex) {
-            Universal.get().getLogger().severe("ERROR: Failed to configure data source!");
-            Universal.get().getLogger().fine(ex.getMessage());
-            return;
+            if (dataSource == null) {
+                throw new IllegalStateException("Failed to generate DataSource.");
+            }
+            Universal.get().getLogger().info("Database initialized using " + (useMySQL ? "MySQL" : "HSQLDB"));
+        } catch (Exception ex) {
+            Universal.get().getLogger().severe("Database setup failed: " + ex.getMessage());
+            Universal.get().debugException(ex);
+            return; // Stop setup on failure
         }
 
+        // Create required tables
         executeStatement(SQLQuery.CREATE_TABLE_PUNISHMENT);
         executeStatement(SQLQuery.CREATE_TABLE_PUNISHMENT_HISTORY);
     }
 
     /**
-     * Shuts down the HSQLDB if used.
+     * Shuts down the database safely.
      */
     public void shutdown() {
+        if (dataSource == null) return;
+
         if (!useMySQL) {
-            try(Connection connection = dataSource.getConnection(); final PreparedStatement statement = connection.prepareStatement("SHUTDOWN")){
-                statement.execute();
-            }catch (SQLException | NullPointerException exc){
-                Universal.get().getLogger().warning("An unexpected error has occurred turning off the database");
-                Universal.get().debugException(exc);
+            try (Connection conn = dataSource.getConnection();
+                 PreparedStatement stmt = conn.prepareStatement("SHUTDOWN")) {
+                stmt.execute();
+                Universal.get().getLogger().info("HSQLDB shutdown successfully.");
+            } catch (SQLException ex) {
+                Universal.get().getLogger().warning("Failed to shutdown HSQLDB: " + ex.getMessage());
+                Universal.get().debugException(ex);
             }
         }
 
-        dataSource.close();
-    }
-    
-    private CachedRowSet createCachedRowSet() throws SQLException {
-    	if (factory == null) {
-    		factory = RowSetProvider.newFactory();
-    	}
-    	return factory.createCachedRowSet();
-    }
-
-    /**
-     * Execute a sql statement without any results.
-     *
-     * @param sql        the sql statement
-     * @param parameters the parameters
-     */
-    public void executeStatement(SQLQuery sql, Object... parameters) {
-        executeStatement(sql, false, parameters);
-    }
-
-    /**
-     * Execute a sql statement.
-     *
-     * @param sql        the sql statement
-     * @param parameters the parameters
-     * @return the result set
-     */
-    public ResultSet executeResultStatement(SQLQuery sql, Object... parameters) {
-        return executeStatement(sql, true, parameters);
-    }
-
-    private ResultSet executeStatement(SQLQuery sql, boolean result, Object... parameters) {
-        return executeStatement(sql.toString(), result, parameters);
-    }
-
-    private synchronized ResultSet executeStatement(String sql, boolean result, Object... parameters) {
-    	try (Connection connection = dataSource.getConnection(); PreparedStatement statement = connection.prepareStatement(sql)) {
-
-    		for (int i = 0; i < parameters.length; i++) {
-    			statement.setObject(i + 1, parameters[i]);
-    		}
-
-    		if (result) {
-    			CachedRowSet results = createCachedRowSet();
-    			results.populate(statement.executeQuery());
-    			return results;
-    		}
-   			statement.execute();
-    	} catch (SQLException ex) {
-    		Universal.get().getLogger().severe(
-   					"An unexpected error has occurred executing an Statement in the database\n"
-   							+ "Please check the plugins/AdvancedBanX/logs/latest.log file and report this "
-    						+ "error in: https://github.com/hlpdev/AdvancedBanX/issues/new"
-    				);
-    		Universal.get().getLogger().fine("Query: \n" + sql);
-    		Universal.get().debugSqlException(ex);
-       	} catch (NullPointerException ex) {
-            Universal.get().getLogger().severe(
-                    "An unexpected error has occurred connecting to the database\n"
-                            + "Check if your MySQL data is correct and if your MySQL-Server is online\n"
-                            + "Please check the plugins/AdvancedBanX/logs/latest.log file and report this "
-                            + "error in: https://github.com/hlpdev/AdvancedBanX/issues/new"
-            );
+        try {
+            dataSource.close();
+            Universal.get().getLogger().info("Database connection pool closed.");
+        } catch (Exception ex) {
+            Universal.get().getLogger().warning("Failed to close DataSource: " + ex.getMessage());
             Universal.get().debugException(ex);
         }
+    }
+
+    private CachedRowSet createCachedRowSet() throws SQLException {
+        if (factory == null) factory = RowSetProvider.newFactory();
+        return factory.createCachedRowSet();
+    }
+
+    public void executeStatement(SQLQuery sql, Object... parameters) {
+        executeStatement(sql.toString(), false, parameters);
+    }
+
+    public ResultSet executeResultStatement(SQLQuery sql, Object... parameters) {
+        return executeStatement(sql.toString(), true, parameters);
+    }
+
+    private synchronized ResultSet executeStatement(String sql, boolean expectResult, Object... parameters) {
+        if (dataSource == null) {
+            Universal.get().getLogger().severe("Cannot execute query: DataSource is not initialized!");
+            return null;
+        }
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            for (int i = 0; i < parameters.length; i++) {
+                stmt.setObject(i + 1, parameters[i]);
+            }
+
+            if (expectResult) {
+                CachedRowSet rowSet = createCachedRowSet();
+                rowSet.populate(stmt.executeQuery());
+                return rowSet;
+            } else {
+                stmt.execute();
+            }
+        } catch (SQLException ex) {
+            Universal.get().getLogger().severe("SQL execution error: " + ex.getMessage());
+            Universal.get().getLogger().fine("Query: " + sql);
+            Universal.get().debugSqlException(ex);
+        }
+
         return null;
     }
 
-    /**
-     * Check whether there is a valid connection to the database.
-     *
-     * @return whether there is a valid connection
-     */
     public boolean isConnectionValid() {
-        return dataSource.isRunning();
+        return dataSource != null && !dataSource.isClosed();
     }
 
-    /**
-     * Check whether MySQL is actually used.
-     *
-     * @return whether MySQL is used
-     */
     public boolean isUseMySQL() {
         return useMySQL;
     }
--- core/src/main/java/net/hnt8/advancedban/utils/utils/DynamicDataSource.java
+++ core/src/main/java/net/hnt8/advancedban/utils/utils/DynamicDataSource.java
@@ -2,41 +2,52 @@ package net.hnt8.advancedban.utils;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import net.hnt8.advancedban.utils.MethodInterface;
-import net.hnt8.advancedban.utils.Universal;
 
 public class DynamicDataSource {
-    private HikariConfig config = new HikariConfig();
+    private final HikariConfig config = new HikariConfig();
 
     public DynamicDataSource(boolean preferMySQL) throws ClassNotFoundException {
         MethodInterface mi = Universal.get().getMethods();
+
         if (preferMySQL) {
             String ip = mi.getString(mi.getMySQLFile(), "MySQL.IP", "Unknown");
             String dbName = mi.getString(mi.getMySQLFile(), "MySQL.DB-Name", "Unknown");
             String usrName = mi.getString(mi.getMySQLFile(), "MySQL.Username", "Unknown");
             String password = mi.getString(mi.getMySQLFile(), "MySQL.Password", "Unknown");
-            String properties = mi.getString(mi.getMySQLFile(), "MySQL.Properties", "verifyServerCertificate=false&useSSL=false&useUnicode=true&characterEncoding=utf8");
+            String properties = mi.getString(mi.getMySQLFile(), "MySQL.Properties",
+                    "useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=utf8");
             int port = mi.getInteger(mi.getMySQLFile(), "MySQL.Port", 3306);
 
             Class.forName("com.mysql.cj.jdbc.Driver");
-            config.setJdbcUrl(
-                    "jdbc:mysql://" + ip + ":" + port + "/" + dbName +
-                            "?useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=utf8"
-            );
+            config.setJdbcUrl("jdbc:mysql://" + ip + ":" + port + "/" + dbName + "?" + properties);
             config.setUsername(usrName);
             config.setPassword(password);
         } else {
-            // No need to worry about relocation because the maven-shade-plugin also changes strings
-            String driverClassName = "org.hsqldb.jdbc.JDBCDriver";
-            Class.forName(driverClassName);
-            config.setDriverClassName(driverClassName);
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            config.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
             config.setJdbcUrl("jdbc:hsqldb:file:" + mi.getDataFolder().getPath() + "/data/storage;hsqldb.lock_file=false");
             config.setUsername("SA");
             config.setPassword("");
         }
+
+        // HikariCP pool settings
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(10000);
+        config.setIdleTimeout(300000);
+        config.setMaxLifetime(600000);
+        config.setPoolName("AdvancedBanPool");
     }
 
-    public HikariDataSource generateDataSource(){
-        return new HikariDataSource(config);
+    public HikariDataSource generateDataSource() {
+        try {
+            HikariDataSource ds = new HikariDataSource(config);
+            ds.getConnection().close(); // Test connection immediately
+            return ds;
+        } catch (Exception ex) {
+            Universal.get().getLogger().severe("Failed to initialize database connection: " + ex.getMessage());
+            Universal.get().debugException(ex);
+            return null;
+        }
     }
 }
